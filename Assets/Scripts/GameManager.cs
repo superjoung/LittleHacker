535768e2d797b52b7dfedc5c644a3a9bb9d3d522
superjoung <46063485+superjoung@users.noreply.github.com>
2024-09-01 21:39:23 +0900

    V0.2.6
    TextManger추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Util;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,24 +14,16 @@ public class GameManager : MonoBehaviour
     static public int playerTurn = 0;
     static public float gridSize = 1;
     static public bool isClear = false;
-    static public bool isTextSkip = false;
 
     public GameObject Canvas; // 플레이어 화면보다 아래에 랜더링 되는 UI
     public GameObject UpperCanvas; // 플레이어 화면보다 위에 랜더링 되는 UI
     public GameObject UiCamera;
-    public TMP_Text talkTextBox;
-
-    public List<Dictionary<string, object>> deClear_Text;
-    public List<Dictionary<string, object>> clear_Text;
-    static public bool talkStart = false;
-    public float textPrintSpeed = 0.1f;
-    private List<string> currentTexts = new List<string>();
-    private int currentTextCount = 0;
-    private bool isPrint = false;
-    private bool talkAnchor = false;
 
     public static Player player;
     public static MapCreate mapCreate;
+
+    private TouchUtil.ETouchState touchState;
+    private Vector2 touchPos;
 
     private void Awake()
     {
@@ -44,26 +37,19 @@ public class GameManager : MonoBehaviour
         player = GameObject.FindWithTag("Player").GetComponent<Player>();
         mapCreate = transform.GetComponent<MapCreate>();
         isClear = false;
-
-        deClear_Text = CSVReader.Read("SN_" + currentScenario.ToString());
-        clear_Text   = CSVReader.Read("SN_" + currentScenario.ToString() + "_Clear");
     }
 
     private void Update()
     {
-        if (talkStart)
-        {
-            if (!talkAnchor)
-            {
-                talkAnchor = true;
-                player = GameObject.FindWithTag("Player").GetComponent<Player>();
-                player.Initialized();
-                TalkStart();
-            }
-            TexControl();
-        }
+        TouchUtil.TouchSetUp(ref touchState, ref touchPos);
 
-        if (!talkStart && isClear) StageClear();
+        if (Managers.Text.isTalk)
+        {
+            if(touchState == TouchUtil.ETouchState.Ended)
+            {
+                Managers.Text.StartTalk();
+            }
+        }
     }
 
     void SpawnInitialized()
@@ -71,97 +57,15 @@ public class GameManager : MonoBehaviour
         Canvas canvas = Instantiate(Canvas).GetComponent<Canvas>();
         Instantiate(UpperCanvas).GetComponent<Canvas>().worldCamera = Camera.main;
         canvas.worldCamera = Instantiate(UiCamera).GetComponent<Camera>();
-        talkTextBox = GameObject.Find("TalkText").GetComponent<TMP_Text>();
     }
 
     // 각각 스크립트 Initialized 함수를 모아서 실행시킬예정
-    public void StageClear()
+    public static void StageClear()
     {
         isClear = false;
         currentStage++;
         PlayerPrefs.SetInt("" + currentScenario.ToString() + "-" + currentStage.ToString(), 1);
         mapCreate.Initialize("SN_" + currentScenario.ToString() + "_ST_" + currentStage.ToString());
-    }
-
-    public void TalkStart()
-    {
-        if (!isClear)
-        {
-            for (int count = 0; count < deClear_Text.Count; count++)
-            {
-                if (deClear_Text[count]["Stage"].ToString() == currentStage.ToString())
-                {
-                    currentTexts.Add(deClear_Text[count]["Talk"].ToString());
-                }
-            }
-        }
-        else
-        {
-            for (int count = 0; count < clear_Text.Count; count++)
-            {
-                if (clear_Text[count]["Stage"].ToString() == currentStage.ToString())
-                {
-                    currentTexts.Add(clear_Text[count]["Talk"].ToString());
-                }
-            }
-        }
-        textPrintSpeed = 0.1f;
-        if (currentTexts.Count != 0) StartCoroutine(printText());
-        else
-        {
-            talkStart = false;
-            talkAnchor = false;
-        }
-    }
-
-    void TexControl()
-    {
-        if(player.playerTouch == Player.ETouchState.End)
-        {
-            if (isPrint)
-            {
-                textPrintSpeed = 0;
-            }
-
-            else
-            {
-                textPrintSpeed = 0.1f;
-                ++currentTextCount;
-                if (currentTextCount == currentTexts.Count)
-                {
-                    talkAnchor = false;
-                    currentTexts.Clear();
-                    currentTextCount = 0;
-                    talkTextBox.text = "";
-                    talkStart = false;
-                }
-
-                else
-                {
-                    StartCoroutine(printText());
-                }
-            }
-        }
-    }
-
-    IEnumerator printText()
-    {
-        Debug.Log("start : " + currentTextCount.ToString());
-        isPrint = true;
-        talkTextBox.text = "";
-        int count = 0;
-        string text = currentTexts[currentTextCount].ToString();
-
-        while(count != text.Length)
-        {
-            if(count < text.Length)
-            {
-                talkTextBox.text += text[count].ToString();
-                count++;
-            }
-            yield return new WaitForSeconds(textPrintSpeed);
-        }
-
-        isPrint = false;
+        Managers.Text.StartTalk();
     }
 }
